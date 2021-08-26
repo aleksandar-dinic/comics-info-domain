6786fd1517147217b64e0ca25e882563e96f6be2
Aleksandar Dinic <a.dinic@icloud.com>
2021-08-26 12:27:56 +0200

    Add startYear and endYear to SeriesSummary

--- Sources/Domain/Series/SeriesSummary.swift
+++ Sources/Domain/Series/SeriesSummary.swift
@@ -25,18 +25,28 @@ public struct SeriesSummary: Codable {
     /// A short bio or description of the series.
     public let description: String?
     
+    /// The first year of publication for the series.
+    public let startYear: Int?
+
+    /// The last year of publication for the series (conventionally, nil for ongoing series).
+    public let endYear: Int?
+    
     public init(
         identifier: String,
         popularity: Int,
         title: String,
         thumbnail: String?,
-        description: String?
+        description: String?,
+        startYear: Int?,
+        endYear: Int?
     ) {
         self.identifier = identifier
         self.popularity = popularity
         self.title = title
         self.thumbnail = thumbnail
         self.description = description
+        self.startYear = startYear
+        self.endYear = endYear
     }
     
 }
